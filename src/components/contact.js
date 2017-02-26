af6ea26bcdad25adf1b4f8cadf5137c8525fb712
Jan Roesner <jan@roesner.it>
2017-02-26 04:48:54 +0100

    Component: use state to render

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react'
 
 export default class Contact extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      firstName: 'Donald',
+      lastName: 'Duck',
+      age: 76
+    }
+  }
+
   render() {
+    const { firstName, lastName, age } = this.state
+
     return(
-      <div>I am a Contact</div>
+      <div>
+        <span>{`Firstname: ${firstName}`} - </span>
+        <span>{`Lastname: ${lastName}`} - </span>
+        <span>{`Age: ${age}`}</span>
+      </div>
     )
   }
 }

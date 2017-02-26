255c29de63fb22e944c3c9ac6ce3b62a49be677f
Jan Roesner <jan@roesner.it>
2017-02-26 04:42:36 +0100

    Component: basic rendering

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Contact from './components/contact'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Lets go
+        <Contact />
       </div>
     );
   }
---
+++ src/components/contact.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class Contact extends Component {
+  render() {
+    return(
+      <div>I am a Contact</div>
+    )
+  }
+}
